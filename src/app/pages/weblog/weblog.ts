c138461b357436ccf04318808068f39819104a38
JunTVDE <juntvde@gmail.com>
2025-09-07 13:08:48 +0200

    added initial data event, added typing, minor UI improvements

--- src/app/pages/weblog/weblog.ts
+++ src/app/pages/weblog/weblog.ts
@@ -1,89 +1,21 @@
-import {AfterViewChecked, Component, effect, ElementRef, signal, ViewChild} from '@angular/core';
+import {Component, effect, ElementRef, OnInit, signal, ViewChild} from '@angular/core';
 import {DatePipe} from '@angular/common';
 import {NgxTypewriterComponent} from '@omnedia/ngx-typewriter';
 import {Header} from '../../partials/header/header';
 import {Footer} from '../../partials/footer/footer';
 import {Socket} from 'ngx-socket-io';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-weblog',
-  imports: [
-    DatePipe,
-    NgxTypewriterComponent,
-    Header,
-    Footer
-  ],
+  imports: [DatePipe, NgxTypewriterComponent, Header, Footer],
   templateUrl: './weblog.html',
   styleUrl: './weblog.css',
 })
 export class Weblog {
-  @ViewChild('weblogList', {static:false}) weblogScroll!: ElementRef;
+  @ViewChild('weblogList', {static: false}) weblogScroll!: ElementRef;
 
-  entries = signal([
-    {
-      date: new Date(),
-      session: '9678e7334ae3',
-      server: 'srv01',
-      country: 'DE',
-      ip: '49.53.XX.XXX',
-      username: 'root',
-      password: 'password',
-    },
-    {
-      date: new Date(),
-      session: '9678e7334ae3',
-      server: 'srv01',
-      country: 'DE',
-      ip: '49.53.XX.XXX',
-      username: 'root',
-      password: 'password',
-    },
-    {
-      date: new Date(),
-      session: '9678e7334ae3',
-      server: 'srv01',
-      country: 'DE',
-      ip: '49.53.XX.XXX',
-      username: 'root',
-      password: 'password',
-    },
-    {
-      date: new Date(),
-      session: '9678e7334ae3',
-      server: 'srv01',
-      country: 'DE',
-      ip: '49.53.XX.XXX',
-      username: 'root',
-      password: 'password',
-    },
-    {
-      date: new Date(),
-      session: '9678e7334ae3',
-      server: 'srv01',
-      country: 'DE',
-      ip: '49.53.XX.XXX',
-      username: 'root',
-      password: 'password',
-    },
-    {
-      date: new Date(),
-      session: '9678e7334ae3',
-      server: 'srv01',
-      country: 'DE',
-      ip: '49.53.XX.XXX',
-      username: 'root',
-      password: 'password',
-    },
-    {
-      date: new Date(),
-      session: '9678e7334ae3',
-      server: 'srv01',
-      country: 'DE',
-      ip: '49.53.XX.XXX',
-      username: 'rootdsuhf98z23qkdsfsojij',
-      password: 'passwordpasswordpasswordpassword',
-    }
-  ]);
+  entries = signal<LoginEvent[]>([]);
 
   constructor(private socket: Socket) {
     effect(() => {
@@ -93,26 +25,32 @@ export class Weblog {
       })
     });
 
-    this.socket.fromEvent<{ session: string, sensor: string, src_ip: string, username: string, password: string }>('login.failed').subscribe((payload) => {
-      this.entries.update(values => [...values, {
-        date: new Date(),
-        session: payload.session,
-        server: payload.sensor,
-        country: 'DE',
-        ip: payload.src_ip,
-        username: payload.username,
-        password: payload.password,
-      }])
-    })
+    // listen to new login attempts
+    this.socket.fromEvent<LoginEvent>('login.failed')
+      .pipe(takeUntilDestroyed())
+      .subscribe((payload: LoginEvent) => {
+        this.entries.update(values => [...values, {
+          date: new Date(),
+          session: payload.session,
+          sensor: payload.sensor,
+          country: payload.country,
+          src_ip: payload.src_ip,
+          username: payload.username,
+          password: payload.password,
+        }])
+      });
+
+    // get initial data
+    this.socket.once('initial-data', (data: LoginEvent[]) => {
+      this.entries.set(data);
+    });
   }
 
   private scrollToBottom() {
     if (this.weblogScroll) {
       this.weblogScroll.nativeElement.scroll({
-        top: this.weblogScroll.nativeElement.scrollHeight,
-        left: 0,
-        behavior: 'smooth',
-      })
+        top: this.weblogScroll.nativeElement.scrollHeight, left: 0, behavior: 'smooth',
+      });
     }
   }
 }
---
+++ src/app/types/login-event.ts
@@ -0,0 +1,9 @@
+type LoginEvent = {
+  date: Date;
+  session: string;
+  sensor: string;
+  src_ip: string;
+  country: string;
+  username: string;
+  password: string;
+}
